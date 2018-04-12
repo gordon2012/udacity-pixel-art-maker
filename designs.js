237009ba1a5fe48f076e0dbd903891ff2858b8b9
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-04-11 20:12:46 -0700

    Add css grid layout WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- designs.js
+++ designs.js
@@ -8,26 +8,44 @@ var width = $('#inputWeight');
 var canvas = $('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+
+// XXX
 $('form input[type="submit"]').on('click', function(evt) {
     evt.preventDefault();
     makeGrid(Number(height.val()), Number(width.val()));
 });
 
-function makeGrid(height, width) {
+
+
+
+$('input[type="number"]').on('input', function(evt) {
+    makeGrid();
+});
+$('input[type="number"]').on('keyup', function(evt) {
+    makeGrid();
+});
+
+
+
+
+function makeGrid() {
     // Your code goes here!
     canvas.empty();
 
+    var h = Number(height.val());
+    var w = Number(width.val());
+
     var elements = '';
-    for(var i = 0; i < height; i++) {
+    for(var i = 0; i < h; i++) {
         elements += '<tr>';
-        for(var j = 0; j < width; j++) {
+        for(var j = 0; j < w; j++) {
             elements += '<td></td>';
         }
         elements += '</tr>';
     }
     canvas.append(elements);
 }
-makeGrid(20, 20);
+makeGrid();
 
 canvas.on('mousedown', 'td', function(evt) {
     $(evt.target).css({'background': color.val()});
